62810ea694c994976a765d450d6f6603a5a57f41
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-11 14:36:09 +0300

    create settings and init

---
+++ core/settings.py
@@ -0,0 +1,56 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, PostgresDsn
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    endpoint: str = "/endpoint"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class LoggingConfig(BaseModel):
+    format: str
+
+
+class DataBase(BaseModel):
+    url: PostgresDsn
+    echo: bool
+    echo_pool: bool
+    pool_size: int
+    max_overflow: int
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class LaunchConfig(BaseModel):
+    app: str
+    host: str
+    port: int
+    reload: bool
+    workers: int
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI__"
+    )
+    conf: LaunchConfig
+    db: DataBase
+    log: LoggingConfig
+    api: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
