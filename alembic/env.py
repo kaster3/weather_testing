a00fe43d39b72900a21b76dbebea69d2aa1a2853
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-21 08:44:55 +0300

    isort fix

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,6 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-
 from core import settings
 from core.database.models.base import Base
 
