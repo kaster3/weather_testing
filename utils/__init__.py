4131b14bfb113c0c14ad3348bad07c76844e99d1
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-23 13:23:27 +0300

    move up pluralize function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,7 @@
-__all__ = ("camel_case_to_snake_case",)
+__all__ = (
+    "camel_case_to_snake_case",
+    "pluralize",
+)
 
 from .case_convector import camel_case_to_snake_case
+from .pluralize import pluralize
