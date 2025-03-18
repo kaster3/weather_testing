94c4288e041331596c48a4fadb5c34e2057b1e24
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-18 06:36:16 +0300

    import this to base model directly

--- app/core/settings.py
+++ app/core/settings.py
@@ -32,14 +32,6 @@ class DataBase(BaseModel):
     pool_size: int
     max_overflow: int
 
-    naming_convention: dict[str, str] = {
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
-    }
-
 
 class RunConfig(BaseModel):
     app: str
@@ -68,6 +60,3 @@ class Settings(BaseSettings):
     db: DataBase
     logging: LoggingConfig
     api: ApiPrefix = ApiPrefix()
-
-
-settings = Settings()
