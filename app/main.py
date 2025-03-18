4a8abb4168869f1ad004d037f5b1294cb2549381
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-18 06:23:15 +0300

    addd gunicorn as manager of gunicorn's workers and put all code in functions

--- app/main.py
+++ app/main.py
@@ -1,44 +1,58 @@
 import logging
 from contextlib import asynccontextmanager
 
-import uvicorn
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from app.api import router as api_router
-from app.core import settings
+from app.core import Settings
+from app.core.gunicorn import Application, get_app_options
+from app.ioc.init_container import init_async_container
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logging.info("Application starts successfully!")
+    yield
+    await app.state.dishka_container.close()
+    logging.info("Application ends successfully!")
+
+
+def create_fastapi_app() -> FastAPI:
+    """Создаем FastAPI приложение и настраиваем его на роутеры"""
+    app = FastAPI(
+        default_response_class=ORJSONResponse,
+        lifespan=lifespan,
+    )
+    app.include_router(
+        router=api_router,
+    )
+    return app
+
+
+def create_app(settings: Settings) -> FastAPI:
+    """Настраиваем логи, контейнер с зависимостями и инициализируем наше приложение"""
     logging.basicConfig(
         level=logging.INFO,
         format=settings.logging.log_format,
     )
-    logging.info("Application starts successfully!")
-    yield
-    logging.info("Application ends successfully!")
+    app = create_fastapi_app()
+    container = init_async_container(settings=settings)
+    setup_dishka(container, app)
+    return app
 
 
-application = FastAPI(
-    default_response_class=ORJSONResponse,
-    lifespan=lifespan,
-)
-
-application.include_router(
-    router=api_router,
-)
-
-
-def main() -> None:
-    uvicorn.run(
-        app=settings.conf.app,
-        host=settings.conf.host,
-        port=settings.conf.port,
-        workers=settings.conf.workers,
-        reload=settings.conf.reload,
-    )
-
-
+# Запускаем приложение с uvicorn воркерами и gunicorn менеджера
 if __name__ == "__main__":
-    main()
+    settings = Settings()
+    Application(
+        application=create_app(settings=settings),
+        options=get_app_options(
+            host=settings.gunicorn.host,
+            port=settings.gunicorn.port,
+            timeout=settings.gunicorn.timeout,
+            workers=settings.gunicorn.workers,
+            log_level=settings.logging.log_level,
+        ),
+    ).run()
